ca11ab291d34ad8f2e16cdd1adb92e2c8c0bcfb4
MariamAtefMah <meroatef451@gmail.com>
2023-12-13 16:58:52 +0200

    Creating the color constant file and import it in StartGameScreen file

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//(./Components) means we go to antother folder in the same directory.
 import Header from "./Components/Header";
+import StartGameScreen from "./Screens/StartGameScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header title="Guess a Number"/>
+      <StartGameScreen/>
     </View>
   );
 }
--- Screens/StartGameScreen.js
+++ Screens/StartGameScreen.js
@@ -1,3 +1,54 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
+//(../Components) means we go back one step to enter another folder in the same directory.
+import Colors from "../Components/Constants/Colors";
+
+const StartGameScreen = (Props) => {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a New Game!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput keyboardType="numeric"/>
+        <View style={styles.buttonConatianer}>
+            <Button title="Reset" onPress={() => {}} color={Colors.accent}/>
+            <Button title="Confirm" onPress={() => {}} color={Colors.primary}/>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        elevation: 10, //this elevation make the shadow styles appear on android.
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+    },
+    buttonConatianer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+    },
+});
+
+export default StartGameScreen;
