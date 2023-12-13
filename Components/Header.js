ab99f30a458f3df857c015303c0fe7a23f36e86b
MariamAtefMah <meroatef451@gmail.com>
2023-12-13 15:05:04 +0200

    Creating components and screen folders and creating Header component

---
+++ Components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+//take care in writting title in (porps.title) as it will be used as it is in App.js file.
+const Header = (Props) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>{Props.title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: 80,
+        paddingTop: 36,
+        backgroundColor: '#f7287b',
+        alignItems: 'center'
+    },
+    headerTitle: {
+        color: 'black',
+        fontSize: 18,
+    },
+});
+
+export default Header;
